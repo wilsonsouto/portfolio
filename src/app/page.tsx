6696d62db9b1be11c83e1382ef398e8e35d16e31
Wilson Souto <wilsonsouto@outlook.com.br>
2024-04-27 18:37:31 -0300

    Implement header with dynamic active section highlighting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,44 @@
-export default function Home() {
+'use client';
+import React, { useState } from 'react';
+
+export default function Header() {
+  const [activeSection, setActiveSection] = useState('about');
+  const [position, setPosition] = useState('0');
+
+  const handleClick = (section: string, position: string) => {
+    setActiveSection(section);
+    setPosition(position);
+  };
+
   return (
-    <>
-      <h1>Next.js Starter Kit</h1>
-    </>
+    <div className='m-auto flex max-w-7xl gap-52 px-10 py-20'>
+      <header className='relative'>
+        <nav className='fixed py-2'>
+          <ul className={`flex min-w-36 flex-col gap-6 text-xl`}>
+            <span
+              style={{ top: `${position}%` }}
+              className={`absolute left-0 -z-10 h-1/5 w-full rounded-md bg-accent-300 transition-all duration-300 ease-in-out`}
+            ></span>
+            {menu.map((item, index) => (
+              <li
+                key={index}
+                className={`cursor-pointer px-4 duration-300 ${item.section === activeSection ? 'text-neutral-900' : 'hover:text-accent-300 dark:text-neutral-400'}`}
+                onClick={() => handleClick(item.section, item.position)}
+              >
+                {item.section}
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </header>
+    </div>
   );
 }
+
+const menu = [
+  { section: 'about', position: '0' },
+  { section: 'experiences', position: '20' },
+  { section: 'education', position: '41' },
+  { section: 'projects', position: '62' },
+  { section: 'contact', position: '82' },
+];
