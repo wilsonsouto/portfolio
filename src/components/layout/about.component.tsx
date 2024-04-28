bfc0263039658cd22d4d6f1438033f0a63543e90
Wilson Souto <wilsonsouto@outlook.com.br>
2024-04-28 10:45:44 -0300

    Refine introduction in About component and add index.css to hand wave animation

--- src/components/layout/about.component.tsx
+++ src/components/layout/about.component.tsx
@@ -1,20 +1,24 @@
 import { Heading, Paragraph } from '../typography';
+import './index.css';
 
 export default function About() {
   return (
     <>
       <Heading text='About' />
       <Paragraph>
-        Hello my name is Wilson Souto, passionate for what I do and committed to delivering the best result, experience
-        in front-end and back-end, allowing me to work on various fronts in Software Development field.
+        Hey <span className='wave'>👋🏻</span>, my name is Wilson Souto.
       </Paragraph>
       <Paragraph>
-        I believe that constant learning is the key to becoming an exceptional professional, and also that technology
-        plays a crucial role in solving problems and improving people's quality of life. My goal is to contribute to
-        this through my work.
+        I'm passionate about what I do and committed to delivering the best results. With experience in both front-end
+        and back-end development, I've been able to tackle various challenges in the field of software development.
       </Paragraph>
       <Paragraph>
-        Besides my work, I have interested in philosophy, music, chess and I don't dismiss a good coffee.
+        I firmly believe that continuous learning is the key to becoming an exceptional professional. Moreover, I see
+        technology as a powerful tool for solving problems and enhancing people's quality of life. My goal is to
+        contribute to this mission through my work.
+      </Paragraph>
+      <Paragraph>
+        Outside of work, I'm interested in philosophy, music, and chess. Oh, and I never say no to a good cup of coffee.
       </Paragraph>
     </>
   );
