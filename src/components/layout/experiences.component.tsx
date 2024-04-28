3241f218375e5885ca6a193168502bc8b031e0da
Wilson Souto <wilsonsouto@outlook.com.br>
2024-04-28 15:16:10 -0300

    Replace h3 tag with Subtitle component and change span color

--- src/components/layout/experiences.component.tsx
+++ src/components/layout/experiences.component.tsx
@@ -1,4 +1,4 @@
-import { Heading, Paragraph } from '../typography';
+import { Heading, Paragraph, Subtitle } from '../typography';
 
 interface ExperiencesProps {
   company: string;
@@ -22,9 +22,9 @@ export default function Experiences() {
       <Heading text='Experiences' />
       {experiences.map((item, index) => (
         <div className='flex flex-col gap-4' key={index}>
-          <h3 className='text-xl text-accent-300'>
+          <Subtitle>
             {item.company} - {item.role}
-          </h3>
+          </Subtitle>
           <span className='italic text-neutral-400'>
             {item.startDate} - {item.endDate}
           </span>
@@ -39,7 +39,7 @@ export default function Experiences() {
             </ul>
           )}
           <Paragraph>
-            <span className='font-bold text-neutral-200'>Skills: </span>
+            <span className='font-bold text-neutral-300'>Skills: </span>
             {item.skills}
           </Paragraph>
           {index !== experiences.length - 1 && <hr className='border-neutral-700 text-accent-600' />}
