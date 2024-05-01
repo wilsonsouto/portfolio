fc2c53ec7d80e2596f07228eeebee47f2ba68421
Wilson Souto <wilsonsouto@outlook.com.br>
2024-05-01 12:50:11 -0300

    Adjust ShadowCard component width

--- src/components/cards/shadow-card.component.tsx
+++ src/components/cards/shadow-card.component.tsx
@@ -8,7 +8,7 @@ interface ShadowCardProps {
 export function ShadowCard(props: ShadowCardProps) {
   return (
     <div
-      className={`${props.marginY} relative flex h-full w-11/12 flex-col rounded-sm bg-accent-200 p-4 text-neutral-900`}
+      className={`${props.marginY} relative flex h-full w-[96%] flex-col rounded-sm bg-accent-200 p-4 text-neutral-900`}
     >
       <div className='absolute left-3 top-3 z-[-1] h-full w-full rounded-sm bg-neutral-950'></div>
       {props.children}
