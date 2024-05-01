2b3af104fac8e75898c784b2a367d7f82fa88e39
Wilson Souto <wilsonsouto@outlook.com.br>
2024-05-01 12:38:04 -0300

    Adjust introduction

--- src/utils/about-data.utils.ts
+++ src/utils/about-data.utils.ts
@@ -5,11 +5,11 @@ interface AboutProps {
 export const aboutData: AboutProps[] = [
   {
     introduction:
-      "I'm passionate about what I do and committed to delivering the best results. With experience in both front-end and back-end development, I've been able to tackle various challenges in the field of software development.",
+      "I'm passionate about what I do and committed to delivering the best results. With experience in front-end and back-end development, I've been able to tackle various challenges in the field of software development.",
   },
   {
     introduction:
-      "I firmly believe that continuous learning is the key to becoming an exceptional professional. Moreover, I see technology as a powerful tool for solving problems and enhancing people's quality of life. My goal is to contribute to this mission through my work.",
+      "I firmly believe that continuous learning is the key to becoming an exceptional professional. Moreover, I see technology as a powerful tool for solving problems and enhancing people's quality of life. My goal is to contribute to this through my work.",
   },
   {
     introduction:
