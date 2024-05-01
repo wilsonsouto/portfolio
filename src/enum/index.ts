c07925c95faa44300aceca172473be2d618ce1a8
Wilson Souto <wilsonsouto@outlook.com.br>
2024-05-01 10:04:06 -0300

    Add Margin enum

---
+++ src/enum/index.ts
@@ -0,0 +1,4 @@
+import { MarginYEnum } from './margin-y.enum';
+import { MarginXEnum } from './margin-y.enum';
+
+export { MarginXEnum, MarginYEnum };
---
+++ src/enum/margin-y.enum.ts
@@ -0,0 +1,17 @@
+export enum MarginYEnum {
+  my0 = 'my-0',
+  my2 = 'my-2',
+  my4 = 'my-4',
+  my6 = 'my-6',
+  my8 = 'my-8',
+  my10 = 'my-10',
+}
+
+export enum MarginXEnum {
+  mx0 = 'mx-0',
+  mx2 = 'mx-2',
+  mx4 = 'mx-4',
+  mx6 = 'mx-6',
+  mx8 = 'mx-8',
+  mx10 = 'mx-10',
+}
