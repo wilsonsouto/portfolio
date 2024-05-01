66bfc3c0a29f30954241a0315b362a6db9ac277b
Wilson Souto <wilsonsouto@outlook.com.br>
2024-05-01 10:41:02 -0300

    Add static file

---
+++ src/app/_static/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function index() {
+  return (
+    <>
+      <div className='my-0'></div>
+      <div className='my-2'></div>
+      <div className='my-4'></div>
+      <div className='my-6'></div>
+      <div className='my-8'></div>
+      <div className='my-10'></div>
+    </>
+  );
+}
