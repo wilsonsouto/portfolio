fed2d5f14932b100ea3e75561df8a75015349209
Wilson Souto <wilsonsouto@outlook.com.br>
2024-12-17 11:09:56 -0300

    feat: add about section component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { HeaderComponent } from '@app/core/components/header/header.component';
+import { AboutComponent } from '@app/core/components/main/about/about.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, HeaderComponent],
+  imports: [CommonModule, HeaderComponent, AboutComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {}
