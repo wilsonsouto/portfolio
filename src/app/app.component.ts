81e426cf51acca97113a454706bc49444b8a80d5
Wilson Souto <wilsonsouto@outlook.com.br>
2024-12-17 12:48:09 -0300

    feat: add education section component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,19 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { HeaderComponent } from '@app/core/components/header/header.component';
 import { AboutComponent } from '@app/core/components/main/about/about.component';
+import { EducationComponent } from '@app/core/components/main/education/education.component';
 import { ExperienceComponent } from '@app/core/components/main/experience/experience.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, HeaderComponent, AboutComponent, ExperienceComponent],
+  imports: [
+    CommonModule,
+    HeaderComponent,
+    AboutComponent,
+    ExperienceComponent,
+    EducationComponent,
+  ],
   templateUrl: './app.component.html',
 })
 export class AppComponent {}
