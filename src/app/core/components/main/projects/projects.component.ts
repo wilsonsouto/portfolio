de65ad4c942abcedacd707515e7ff3742b8e1d80
Wilson Souto <wilsonsouto@outlook.com.br>
2024-12-17 13:12:22 -0300

    feat: add projects section component

---
+++ src/app/core/components/main/projects/projects.component.ts
@@ -0,0 +1,42 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { SectionTitleComponent } from '@app/shared/components/section-title/section-title.component';
+
+interface Project {
+  name: string;
+  href: string;
+  description: string;
+  stacks: string[];
+}
+
+@Component({
+  selector: 'app-projects',
+  standalone: true,
+  imports: [CommonModule, SectionTitleComponent],
+  templateUrl: './projects.component.html',
+})
+export class ProjectsComponent {
+  projects: Project[] = [
+    {
+      name: 'Pizzeria Donna',
+      href: 'https://github.com/wilsonsouto/pizzeria-donna',
+      description:
+        'Discreet chatbot assistance for women victims of domestic violence',
+      stacks: [
+        'IBM Watson Assistant',
+        'Angular',
+        'TypeScript',
+        'HTML',
+        'CSS',
+        'SASS',
+        'GIT',
+      ],
+    },
+    {
+      name: 'File Organizer',
+      href: 'https://github.com/wilsonsouto/file-organizer',
+      description: 'A script that organizes files based on their extensions',
+      stacks: ['C#'],
+    },
+  ];
+}
