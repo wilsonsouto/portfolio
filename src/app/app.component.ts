3c13aa8c4163fa65a8a12dff9da36c2591266b65
Wilson Souto <wilsonsouto@outlook.com.br>
2025-01-01 23:01:26 -0300

    Add ExperienceComponent and DateSpanComponent; update app layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { HeaderComponent } from '@app/core/components/header/header.component';
 import { AboutComponent } from '@app/core/components/main/about/about.component';
+import { ExperienceComponent } from '@app/core/components/main/experience/experience.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, HeaderComponent, AboutComponent],
+  imports: [CommonModule, HeaderComponent, AboutComponent, ExperienceComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {}
