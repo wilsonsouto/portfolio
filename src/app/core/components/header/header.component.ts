2ff3ea7cdb01d268d8a2d03ed560257298783539
Wilson Souto <wilsonsouto@outlook.com.br>
2025-01-13 21:54:27 -0300

    Refactor HeaderComponent applying NavigationLinkComponent

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,36 +1,37 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NavigationLinkComponent } from '@app/shared/components/navigation-link/navigation-link.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NavigationLinkComponent],
   templateUrl: './header.component.html',
 })
 export class HeaderComponent {
   public title: string = 'Wilson Souto';
   public description: string =
-    'Software Developer 🚀 creating value by solving problems and building solutions';
-  public location: { href: string; name: string }[] = [
+    'Software Developer passionate about creating value by solving problems and building solutions';
+  public location: { name: string; url: string }[] = [
     {
-      href: 'https://www.google.com/maps/place/S%C3%A3o+Paulo',
       name: 'São Paulo, SP, Brazil',
+      url: 'https://www.google.com/maps/place/S%C3%A3o+Paulo',
     },
   ];
-  public contactLinks: { href: string; name: string; icon: string }[] = [
+  public contactLinks: { name: string; url: string; icon: string }[] = [
     {
-      href: 'mailto:wilsonsouto@outlook.com.br',
       name: 'Email',
+      url: 'mailto:wilsonsouto@outlook.com.br',
       icon: 'fa-regular fa-envelope',
     },
     {
-      href: 'https://github.com/wilsonsouto',
       name: 'Github',
+      url: 'https://github.com/wilsonsouto',
       icon: 'fa-brands fa-github',
     },
     {
-      href: 'https://linkedin.com/in/wilsonsouto',
       name: 'Linkedin',
+      url: 'https://linkedin.com/in/wilsonsouto',
       icon: 'fa-brands fa-linkedin',
     },
   ];
