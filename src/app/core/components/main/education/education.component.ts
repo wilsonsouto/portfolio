9634b8edcb39d74ed536eb10fdbb8c1cfef47fbc
Wilson Souto <wilsonsouto@outlook.com.br>
2025-01-14 09:34:26 -0300

    Refactor ProjectsComponent applying NavigationLinkComponent

--- src/app/core/components/main/education/education.component.ts
+++ src/app/core/components/main/education/education.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { DateSpanComponent } from '@app/shared/components/date-span/date-span.component';
+import { NavigationLinkComponent } from '@app/shared/components/navigation-link/navigation-link.component';
 import { SectionHeadingComponent } from '@app/shared/components/section-heading/section-heading.component';
 
 interface Education {
   institution: {
     name: string;
-    href: string;
+    url: string;
   };
   startDate: string;
   endDate: string;
@@ -16,7 +17,12 @@ interface Education {
 @Component({
   selector: 'app-education',
   standalone: true,
-  imports: [CommonModule, SectionHeadingComponent, DateSpanComponent],
+  imports: [
+    CommonModule,
+    SectionHeadingComponent,
+    DateSpanComponent,
+    NavigationLinkComponent,
+  ],
   templateUrl: './education.component.html',
 })
 export class EducationComponent {
@@ -24,7 +30,7 @@ export class EducationComponent {
     {
       institution: {
         name: 'FIAP',
-        href: 'https://www.fiap.com.br/',
+        url: 'https://www.fiap.com.br/',
       },
       startDate: '2021',
       endDate: '2022',
