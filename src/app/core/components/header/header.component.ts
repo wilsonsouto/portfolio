3a55378d4dc044bb548629c280e25ceedad5ddda
Wilson Souto <wilsonsouto@outlook.com.br>
2025-01-15 23:53:42 -0300

    Add resume data JSON file and update HeaderComponent to use it

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,6 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { NavigationLinkComponent } from '@app/shared/components/navigation-link/navigation-link.component';
+import * as data from '@app/shared/data/resume-data.json';
+
+interface Contact {
+  label: string;
+  url: string;
+  icon: string;
+}
+
+interface Props {
+  name: string;
+  description: string;
+  location: {
+    place: string;
+    url: string;
+  };
+  contact: Contact[];
+}
 
 @Component({
   selector: 'app-header',
@@ -9,30 +26,15 @@ import { NavigationLinkComponent } from '@app/shared/components/navigation-link/
   templateUrl: './header.component.html',
 })
 export class HeaderComponent {
-  public title: string = 'Wilson Souto';
-  public description: string =
-    'Software Developer passionate about creating value by solving problems and building solutions';
-  public location: { name: string; url: string }[] = [
+  public headerData: Props[] = [
     {
-      name: 'São Paulo, SP, Brazil',
-      url: 'https://www.google.com/maps/place/S%C3%A3o+Paulo',
-    },
-  ];
-  public contactLinks: { name: string; url: string; icon: string }[] = [
-    {
-      name: 'Email',
-      url: 'mailto:wilsonsouto@outlook.com.br',
-      icon: 'fa-regular fa-envelope',
-    },
-    {
-      name: 'Github',
-      url: 'https://github.com/wilsonsouto',
-      icon: 'fa-brands fa-github',
-    },
-    {
-      name: 'Linkedin',
-      url: 'https://linkedin.com/in/wilsonsouto',
-      icon: 'fa-brands fa-linkedin',
+      name: data.name,
+      description: data.description,
+      location: {
+        place: data.location.place,
+        url: data.location.url,
+      },
+      contact: data.contact,
     },
   ];
 }
