feb5cfb42704cf1551d5d7c95ede5ba2f41df236
Wilson Souto <wilsonsouto@outlook.com.br>
2025-01-17 21:58:41 -0300

    Update resume JSON file with SkillsComponent content

--- src/app/core/components/main/skills/skills.component.ts
+++ src/app/core/components/main/skills/skills.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { SectionHeadingComponent } from '@app/shared/components/section-heading/section-heading.component';
+import * as data from '@public/resume-data.json';
 
 interface Props {
   topic: string;
-  isVisible: boolean;
   stacks: string[];
+  isVisible?: boolean;
 }
 
 @Component({
@@ -15,69 +16,10 @@ interface Props {
   templateUrl: './skills.component.html',
 })
 export class SkillsComponent {
-  skills: Props[] = [
-    {
-      topic: 'Languages',
-      isVisible: false,
-      stacks: ['C#', 'HTML', 'CSS/SASS', 'TypeScript', 'Bash (Shell)'],
-    },
-    {
-      topic: 'Frameworks and Libraries',
-      isVisible: false,
-      stacks: [
-        '.NET',
-        'ASP.NET Core',
-        'Entity Framework (EF) Core',
-        'Angular',
-        'Tailwind CSS',
-      ],
-    },
-    {
-      topic: 'Design Principles and Architecture',
-      isVisible: false,
-      stacks: [
-        'SOLID',
-        'Model-View-Controller (MVC)',
-        'Domain-Driven Design (DDD)',
-        'Microservices',
-        'Event-driven Architecture (EDA)',
-      ],
-    },
-    {
-      topic: 'Integration and Messaging',
-      isVisible: false,
-      stacks: ['APIs (RESTful, HTTP, SOAP, GraphQL)', 'RabbitMQ'],
-    },
-    {
-      topic: 'Data Management',
-      isVisible: false,
-      stacks: [
-        'SQL (SQL Server, MySQL)',
-        'NoSQL (MongoDB, Elasticsearch, Redis)',
-      ],
-    },
-    {
-      topic: 'DevOps and Containers',
-      isVisible: false,
-      stacks: ['Kubernetes', 'Jenkins', 'Docker'],
-    },
-    {
-      topic: 'Operating Systems',
-      isVisible: false,
-      stacks: ['Linux (Ubuntu)', 'Windows'],
-    },
-    {
-      topic: 'Development Tools and Methodologies',
-      isVisible: false,
-      stacks: [
-        'Git',
-        'Postman',
-        'Test-Driven Development (TDD)',
-        'Scrum',
-        'Kanban',
-      ],
-    },
-  ];
+  skills: Props[] = data.skills.map((skill) => ({
+    topic: skill.topic,
+    stacks: skill.stacks,
+  }));
 
   toggleVisibility(index: number) {
     this.skills.forEach((skill, i) => {
